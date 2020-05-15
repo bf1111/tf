0c3048ac1f81455a5f941aedb1174717f90cc452
xiaoming <xiaoming@qq.com>
2020-05-15 16:22:43 +0800

    跨域

--- public/index.php
+++ public/index.php
@@ -11,6 +11,10 @@
 
 // [ 应用入口文件 ]
 namespace think;
+//跨域
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
+header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding");
 
 // 加载基础文件
 require __DIR__ . '/../thinkphp/base.php';
